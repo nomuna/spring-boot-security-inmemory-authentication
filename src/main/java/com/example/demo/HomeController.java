6d42ae6d43bed0d09a87bbd2ae538f5889f6359a
nomuna <genarek@gmail.com>
2019-11-27 14:50:24 +0100

    logging statments in controller and .gitignore

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    public static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping("/")
     public String greeting(Authentication authentication){
-        System.out.printf("User : %s", authentication.getPrincipal());
+        if (logger.isDebugEnabled()) {
+            logger.debug("User: {}", authentication.getPrincipal());
+        }
         return "home";
     }
 
     @GetMapping("/admin")
     public String admin(Authentication authentication){
-        System.out.printf("User : %s", authentication.getPrincipal());
+        if (logger.isDebugEnabled()) {
+            logger.debug("User: {}", authentication.getPrincipal());
+        }
         return "admin";
     }
 }
